8669f8a36d3a3f4fe0e8f02cac88cf4753f2a03e
Salvatore Bova <salvatore_bova@hotmail.it>
2019-05-10 14:40:12 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -54,7 +54,8 @@ router.post('/login', (req, res, next) => {
                     if(result){
                       res.cookie('user_id', user.id, {
                         httpOnly: true,
-                        signed: true
+                        signed: true,
+                        secure: true
                         //adding secure remove the cookies from signedCookies
                       });
                       res.json({
