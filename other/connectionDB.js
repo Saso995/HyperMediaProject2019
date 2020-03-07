2493feecdf7091d233552e503fdcb8950caf5bde
Salvatore Bova <salvatore_bova@hotmail.it>
2020-03-07 18:16:26 +0100

    env vars
    adding env vars

--- other/connectionDB.js
+++ other/connectionDB.js
@@ -1,7 +1,7 @@
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    user: 'lgkrsspypjfgyp',
+    user: process.env.DB_USER,
     host: 'ec2-54-246-121-32.eu-west-1.compute.amazonaws.com',
     database: 'deogk8jckp1m9i',
     password: 'fee5dc2195a33dd430be880a29ab15ab8f60e49a756ff40ba5d305408b80cbbf',
