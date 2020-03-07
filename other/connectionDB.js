1476c7117fb71bbd38838b5f76f473775e071723
Salvatore Bova <salvatore_bova@hotmail.it>
2020-03-07 18:20:36 +0100

    Update connectionDB.js

--- other/connectionDB.js
+++ other/connectionDB.js
@@ -2,10 +2,10 @@ const knex = require('knex')({
   client: 'pg',
   connection: {
     user: process.env.DB_USER,
-    host: 'ec2-54-246-121-32.eu-west-1.compute.amazonaws.com',
-    database: 'deogk8jckp1m9i',
-    password: 'fee5dc2195a33dd430be880a29ab15ab8f60e49a756ff40ba5d305408b80cbbf',
-    port: 5432
+    host: process.env.DB_HOST,
+    database: process.env.DB_DB,
+    password: process.env.DB_PSW,
+    port: process.env.DB_PORT
   }
 });
 
